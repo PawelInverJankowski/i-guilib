e1843786e0ddc233b3f3d6645a862b30c57bcc12
Thinkpad <Thinkpad@DESKTOP-UO4EDRF>
2018-11-24 13:08:45 +0100

    Adding Frist Files

---
+++ I-GUILib/src/io/inver/iguilib/container/IFrame.java
@@ -0,0 +1,34 @@
+package io.inver.iguilib.container;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class IFrame extends JFrame 
+{
+
+	public IFrame() throws HeadlessException 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public IFrame(GraphicsConfiguration arg0) 
+	{
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IFrame(String arg0) throws HeadlessException 
+	{
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IFrame(String arg0, GraphicsConfiguration arg1) 
+	{
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ I-GUILib/src/io/inver/iguilib/container/IPanel.java
@@ -0,0 +1,28 @@
+package io.inver.iguilib.container;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class IPanel extends JPanel {
+
+	public IPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public IPanel(LayoutManager arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IPanel(boolean arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IPanel(LayoutManager arg0, boolean arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
